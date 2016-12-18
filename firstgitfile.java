8a626d93e3923c0b8fcb978cea198349f67b9764
javalabel <java_seed@mail.ru>
2016-12-18 01:02:37 +0100

    a

--- firstgitfile.java
+++ firstgitfile.java
@@ -1,15 +1,5 @@
 
-Hello, GIT!!!!!!!!!!!!!!!!!!!!
-Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
-Hello, GIT!
-Hello, GIT!Hello, GIT!
+Hello, GIT!!!!!!!!!!!!!!!!!!!234
 Hello, GIT!Hello, GIT!Hello, GIT!
 Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
 Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!Hello, GIT!
